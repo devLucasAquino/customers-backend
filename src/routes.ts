866e1a6ffc1e4a94050159fcb66bfb9675796883
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-09-30 18:57:38 -0300

    feat: list all customers

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import {
     FastifyReply
 } from 'fastify';
 import { CreateCustomerController } from './controllers/create-customer-controller';
+import { ListCustomerController } from './controllers/list-customer-controller';
 
 export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions){
     fastify.get("/teste", async (request: FastifyRequest, reply: FastifyReply) => {
@@ -13,5 +14,9 @@ export async function routes(fastify: FastifyInstance, options: FastifyPluginOpt
 
     fastify.post("/customer", async (request: FastifyRequest, reply: FastifyReply) => {
         return new CreateCustomerController().handle(request, reply);
+    });
+
+    fastify.get("/customers", async (request: FastifyRequest, reply: FastifyReply) => {
+        return new ListCustomerController().handle(request, reply);
     });
 };
